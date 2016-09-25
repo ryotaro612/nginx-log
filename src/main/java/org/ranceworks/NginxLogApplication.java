3affaf938e35357c0723a102eed254939e8e5226
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-09-25 14:43:34 +0530

    thymeleaf works

--- src/main/java/org/ranceworks/NginxLogApplication.java
+++ src/main/java/org/ranceworks/NginxLogApplication.java
@@ -2,11 +2,21 @@ package org.ranceworks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class NginxLogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NginxLogApplication.class, args);
 	}
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+	
 }
+
