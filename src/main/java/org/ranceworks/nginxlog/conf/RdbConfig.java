11aefaf43289979cb36204914bf1a12e2500061b
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-11-19 18:44:49 +0900

    ignore prod file

--- src/main/java/org/ranceworks/nginxlog/conf/RdbConfig.java
+++
@@ -1,9 +0,0 @@
-package org.ranceworks.nginxlog.conf;
-
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-// TODO base package
-@EnableJpaRepositories
-public class RdbConfig {
-
-}
